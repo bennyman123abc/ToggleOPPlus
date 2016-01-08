99455528909a6e9919ef62062d5eb1079d68e76b
bennyman123abc <bennyman123abc@hotmail.com>
2016-01-08 09:24:02 -0500

    Removed extra Spacing in the main class file (only class file as of now) and updated the version number in the plugin.yml

--- com/bennyman123abc/ToggleOP.java
+++ com/bennyman123abc/ToggleOP.java
@@ -1,93 +1,93 @@
- package com.bennyman123abc;
+package com.bennyman123abc;
  
- import java.util.logging.Logger;
- import org.bukkit.Bukkit;
- import org.bukkit.ChatColor;
- import org.bukkit.Server;
- import org.bukkit.command.Command;
- import org.bukkit.command.CommandSender;
- import org.bukkit.entity.Player;
- import org.bukkit.permissions.Permission;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
  
- public class ToggleOP extends org.bukkit.plugin.java.JavaPlugin
- {
-   public Permission permission = new Permission("top.op");
+public class ToggleOP extends org.bukkit.plugin.java.JavaPlugin
+{
+  public Permission permission = new Permission("top.op");
    
-   public void onEnable()
-   {
-     getLogger().info("Plugin Made by bennyman123abc");
-   }
-   
- 
- 
-   public void onDisable() {}
+  public void onEnable()
+  {
+    getLogger().info("Plugin Made by bennyman123abc");
+  }
    
  
-   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-   {
-     Player player = (Player)sender;
+ 
+  public void onDisable() {}
+   
+ 
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+  {
+    Player player = (Player)sender;
      
-     if ((cmd.getName().equalsIgnoreCase("toggleop")) && (player.hasPermission("top.op")) && ((sender instanceof Player)))
-     {
-       int length = args.length;
+    if ((cmd.getName().equalsIgnoreCase("toggleop")) && (player.hasPermission("top.op")) && ((sender instanceof Player)))
+    {
+      int length = args.length;
        
-       if (length == 0) {
-         if (player.isOp()) {
-           player.sendMessage(ChatColor.GOLD + "You Are No Longer OP");
-           player.setOp(false);
+      if (length == 0) {
+        if (player.isOp()) {
+          player.sendMessage(ChatColor.GOLD + "You Are No Longer OP");
+          player.setOp(false);
  
-         }
-         else if (!player.isOp()) {
-           player.sendMessage(ChatColor.GOLD + "You Are Now OP");
-           player.setOp(true);
-         }
-       }
+        }
+        else if (!player.isOp()) {
+          player.sendMessage(ChatColor.GOLD + "You Are Now OP");
+          player.setOp(true);
+        }
+      }
        
-       if (length == 1)
-       {
-         boolean playerFound = false;
-         
-         for (Player toggleop : Bukkit.getServer().getOnlinePlayers()) {
-           if (toggleop.getName().equalsIgnoreCase(args[0])) {
-             if (toggleop.isOp()) {
-               toggleop.sendMessage(ChatColor.GOLD + player.getName() + " Deopped You");
-               player.sendMessage(ChatColor.GOLD + toggleop.getName() + " Is No Longer OP");
-               toggleop.setOp(false);
-               playerFound = true;
-               break;
-             }
-             if (!toggleop.isOp()) {
-               toggleop.sendMessage(ChatColor.GOLD + player.getName() + " Has Opped You");
-               player.sendMessage(ChatColor.GOLD + toggleop.getName() + " Is Now OP");
-               toggleop.setOp(true);
-               playerFound = true;
-               break;
-             }
-           }
-         }
-         if (!playerFound) {
-           player.sendMessage(ChatColor.GOLD + args[0] + " Is Not Currently Online");
-         }
-       }
-       
-       if (length == 2) {
-         boolean playerFound = false;
-         
-         for (final Player toggleop : Bukkit.getServer().getOnlinePlayers()) {
-           if (toggleop.getName().equalsIgnoreCase(args[0])) {
-             Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-               public void run() {
-                 toggleop.setOp(false);
-               }
-             });
-           }
-         }
-       }
-       if ((cmd.getName().equalsIgnoreCase("toggleop")) && (!player.hasPermission("top.op")) && ((sender instanceof Player))) {
-         player.sendMessage(ChatColor.RED + "You Don't Have Permission To Do That");
-         getLogger().warning(player.getName() + " Tried To Toggle Their OP");
-       }
-     }
-     return false;
-   }
- }
+      if (length == 1)
+      {
+        boolean playerFound = false;
+        
+        for (Player toggleop : Bukkit.getServer().getOnlinePlayers()) {
+          if (toggleop.getName().equalsIgnoreCase(args[0])) {
+            if (toggleop.isOp()) {
+              toggleop.sendMessage(ChatColor.GOLD + player.getName() + " Deopped You");
+              player.sendMessage(ChatColor.GOLD + toggleop.getName() + " Is No Longer OP");
+              toggleop.setOp(false);
+              playerFound = true;
+              break;
+            }
+            if (!toggleop.isOp()) {
+              toggleop.sendMessage(ChatColor.GOLD + player.getName() + " Has Opped You");
+              player.sendMessage(ChatColor.GOLD + toggleop.getName() + " Is Now OP");
+              toggleop.setOp(true);
+              playerFound = true;
+              break;
+            }
+          }
+        }
+        if (!playerFound) {
+          player.sendMessage(ChatColor.GOLD + args[0] + " Is Not Currently Online");
+        }
+      }
+      
+      if (length == 2) {
+        boolean playerFound = false;
+        
+        for (final Player toggleop : Bukkit.getServer().getOnlinePlayers()) {
+          if (toggleop.getName().equalsIgnoreCase(args[0])) {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+              public void run() {
+                toggleop.setOp(false);
+              }
+            });
+          }
+        }
+      }
+      if ((cmd.getName().equalsIgnoreCase("toggleop")) && (!player.hasPermission("top.op")) && ((sender instanceof Player))) {
+        player.sendMessage(ChatColor.RED + "You Don't Have Permission To Do That");
+        getLogger().warning(player.getName() + " Tried To Toggle Their OP");
+      }
+    }
+    return false;
+  }
+}
